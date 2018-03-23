8d24d2eff69ca61e0124251257a1dfd248cb08f2
Francisco <francisco.nascimento@icen.ufpa.br>
2018-03-23 12:10:56 -0300

    Foram realizadas alterações no vhdl, entre elas a inclusão de variáveis e portas para a sdram1, pois até então havia somente para a sdram0. Porém continua sem funcionar para os códigos do eclipse (C/C++).

--- VHDL/lights.vhd
+++ VHDL/lights.vhd
@@ -1,51 +1,71 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.all;
 USE ieee.std_logic_unsigned.ALL;
 
 ENTITY lights IS
 	PORT (
+		KEY : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
 		CLOCK_50 : IN STD_LOGIC;
-		KEY : IN STD_LOGIC_VECTOR (0 DOWNTO 0);
-		DRAM_CLK, DRAM_CKE : OUT STD_LOGIC;
-		DRAM_ADDR : OUT STD_LOGIC_VECTOR(12 DOWNTO 0);
-		DRAM_BA : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
-		DRAM_CS_N, DRAM_CAS_N, DRAM_RAS_N, DRAM_WE_N : OUT STD_LOGIC;
-		DRAM_DQ : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		DRAM_DQM : OUT STD_LOGIC_VECTOR(1 DOWNTO 0) 
-	);
+		DRAM_DQ : INOUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+		DRAM0_ADDR, DRAM1_ADDR : OUT STD_LOGIC_VECTOR (12 DOWNTO 0);
+		DRAM0_BA_1, DRAM0_BA_0, DRAM1_BA_1, DRAM1_BA_0 : BUFFER STD_LOGIC;
+		DRAM0_CAS_N, DRAM0_RAS_N, DRAM0_CLK : OUT STD_LOGIC;
+		DRAM0_CKE, DRAM0_CS_N, DRAM0_WE_N : OUT STD_LOGIC;
+		DRAM0_UDQM, DRAM0_LDQM, DRAM1_UDQM, DRAM1_LDQM : BUFFER STD_LOGIC;
+		DRAM1_CAS_N, DRAM1_RAS_N, DRAM1_CLK : OUT STD_LOGIC;
+		DRAM1_CKE, DRAM1_CS_N, DRAM1_WE_N : OUT STD_LOGIC);
 END lights;
 
-ARCHITECTURE lights_rtl OF lights IS
+ARCHITECTURE Structure OF lights IS
 	COMPONENT nios_system
 		PORT (
-			clk_clk          : in    std_logic                     := 'X';             -- clk
-         reset_reset_n    : in    std_logic                     := 'X';             -- reset_n
-			sdram_clk_clk    : out   std_logic;
-         sdram_wire_addr  : out   std_logic_vector(12 downto 0);                    -- addr
-         sdram_wire_ba    : out   std_logic_vector(1 downto 0);                     -- ba
-         sdram_wire_cas_n : out   std_logic;                                        -- cas_n
-         sdram_wire_cke   : out   std_logic;                                        -- cke
-         sdram_wire_cs_n  : out   std_logic;                                        -- cs_n
-         sdram_wire_dq    : inout std_logic_vector(31 downto 0) := (others => 'X'); -- dq
-         sdram_wire_dqm   : out   std_logic_vector(1 downto 0);                     -- dqm Foi mudado de (3 downto 0 para 1 downto 0)
-         sdram_wire_ras_n : out   std_logic;                                        -- ras_n
-         sdram_wire_we_n  : out   std_logic                                         -- we_n
-		);
+			clk_clk : IN STD_LOGIC;
+			reset_reset_n : IN STD_LOGIC;
+			sdram_clk_clk : OUT STD_LOGIC;
+			sdram_wire_addr : OUT STD_LOGIC_VECTOR(12 DOWNTO 0);
+			sdram_wire_ba : BUFFER STD_LOGIC_VECTOR(1 DOWNTO 0);
+			sdram_wire_cas_n : OUT STD_LOGIC;
+			sdram_wire_cke : OUT STD_LOGIC;
+			sdram_wire_cs_n : OUT STD_LOGIC;
+			sdram_wire_dq : INOUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+			sdram_wire_dqm : BUFFER STD_LOGIC_VECTOR(3 DOWNTO 0);
+			sdram_wire_ras_n : OUT STD_LOGIC;
+			sdram_wire_we_n : OUT STD_LOGIC);
 	END COMPONENT;
+	
+	SIGNAL DQM : STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL BA : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	SIGNAL DRAM_ADDR : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL DRAM_CAS_N, DRAM_RAS_N : STD_LOGIC;
+	SIGNAL DRAM_CLK, DRAM_CKE : STD_LOGIC;
+	SIGNAL DRAM_CS_N, DRAM_WE_N : STD_LOGIC;
+	
 	BEGIN
+		DRAM0_BA_0 <= BA(0); DRAM0_BA_1 <= BA(1);
+		DRAM1_BA_0 <= BA(0); DRAM1_BA_1 <= BA(1);
+		DRAM0_UDQM <= DQM(1); DRAM0_LDQM <= DQM(0);
+		DRAM1_UDQM <= DQM(3); DRAM1_LDQM <= DQM(2);
+		DRAM0_ADDR <= DRAM_ADDR; DRAM1_ADDR <= DRAM_ADDR;
+		DRAM0_CAS_N <= DRAM_CAS_N; DRAM1_CAS_N <= DRAM_CAS_N;
+		DRAM0_CKE <= DRAM_CKE; DRAM1_CKE <= DRAM_CKE;
+		DRAM0_CLK <= DRAM_CLK; DRAM1_CLK <= DRAM_CLK;
+		DRAM0_CS_N <= DRAM_CS_N; DRAM1_CS_N <= DRAM_CS_N;
+		DRAM0_RAS_N <= DRAM_RAS_N; DRAM1_RAS_N <= DRAM_RAS_N;
+		DRAM0_WE_N <= DRAM_WE_N; DRAM1_WE_N <= DRAM_WE_N;
+	
 		NiosII : nios_system
 		PORT MAP(
-			clk_clk          => CLOCK_50,          --        clk.clk
-         reset_reset_n    => KEY(0),    --      reset.reset_n
-			sdram_clk_clk    => DRAM_CLK,
-         sdram_wire_addr  => DRAM_ADDR,  -- sdram_wire.addr
-         sdram_wire_ba    => DRAM_BA,    --           .ba
-         sdram_wire_cas_n => DRAM_CAS_N, --           .cas_n
-         sdram_wire_cke   => DRAM_CKE,   --           .cke
-         sdram_wire_cs_n  => DRAM_CS_N,  --           .cs_n
-         sdram_wire_dq    => DRAM_DQ,    --           .dq
-         sdram_wire_dqm   => DRAM_DQM,   --           .dqm
-         sdram_wire_ras_n => DRAM_RAS_N, --           .ras_n
-         sdram_wire_we_n  => DRAM_WE_N   --           .we_n
-		);
-END lights_rtl;
+			clk_clk => CLOCK_50,
+			reset_reset_n => KEY(0),
+			sdram_clk_clk => DRAM_CLK,
+			sdram_wire_addr => DRAM_ADDR,
+			sdram_wire_ba => BA,
+			sdram_wire_cas_n => DRAM_CAS_N,
+			sdram_wire_cke => DRAM_CKE,
+			sdram_wire_cs_n => DRAM_CS_N,
+			sdram_wire_dq => DRAM_DQ,
+			sdram_wire_dqm => DQM,
+			sdram_wire_ras_n => DRAM_RAS_N,
+			sdram_wire_we_n => DRAM_WE_N);
+END Structure;
